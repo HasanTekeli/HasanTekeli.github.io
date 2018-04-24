7a6e80519c0af9efcc6c072d93e1cf24eafce7fb
HasanTekeli <hasan_tekeli@yahoo.com>
2018-04-24 12:43:28 +0300

    BG Generator added

---
+++ pages/Projects/JS/BgGenerator/index.js
@@ -0,0 +1,41 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var input = document.querySelector("input")
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+//Alttaki geniş hali
+// color1.addEventListener("input", function() {
+// 	body.style.background = 
+// 	"linear-gradient(to right, " 
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+// })
+
+// color2.addEventListener("input", function() {
+// 	body.style.background = 
+// 	"linear-gradient(to right, " 
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+// })
